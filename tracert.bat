d6db201be32067ceda597b696dbff0502ad8bcd1
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2018-10-11 09:57:08 +0200

    Update tracert.bat

--- tracert.bat
+++ tracert.bat
@@ -2,29 +2,30 @@
 
 echo Agarde un intre, o proceso esta en marcha.
 
-tracert www.wikivoyage.org >> C:\Users\B\Desktop\R.txt
-echo **** 10.0.129.217 **** >> C:\Users\B\Desktop\R.txt
+tracert www.wikivoyage.org >> %USERPROFILE%\Desktop\LOG_R.txt
+echo **** 10.0.129.217 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 5
 
-tracert www.mediawiki.org >> C:\Users\B\Desktop\R.txt
-echo **** 10.0.129.224 **** >> C:\Users\B\Desktop\R.txt
+tracert www.mediawiki.org >> %USERPROFILE%\Desktop\LOG_R.txt
+echo **** 10.0.129.224 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 4
 
-tracert www.wikimedia.org >> C:\Users\B\Desktop\R.txt
-echo **** 10.0.129.227 **** >> C:\Users\B\Desktop\R.txt
+tracert www.wikimedia.org >> %USERPROFILE%\Desktop\LOG_R.txt
+echo **** 10.0.129.227 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 3
 
-tracert www.wikidata.org >> C:\Users\B\Desktop\R.txt
-echo **** 10.0.129.229 **** >> C:\Users\B\Desktop\R.txt
+tracert www.wikidata.org >> %USERPROFILE%\Desktop\LOG_R.txt
+echo **** 10.0.129.229 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 2
 
-tracert www.wikipedia.org >> C:\Users\B\Desktop\R.txt
-echo **** 10.0.129.249 **** >> C:\Users\B\Desktop\R.txt
+tracert www.wikipedia.org >> %USERPROFILE%\Desktop\LOG_R.txt
+echo **** 10.0.129.249 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 1
 
-date /t >> C:\Users\B\Desktop\R.txt
-time /t  >> C:\Users\B\Desktop\R.txt
-echo  >> C:\Users\B\Desktop\R.txt
+date /t >> %USERPROFILE%\Desktop\R.txt
+time /t  >> %USERPROFILE%\Desktop\R.txt
+echo  >> %USERPROFILE%\Desktop\R.txt
+
 echo Proceso rematado, pode ver o resultado no log que se creou no seu escritorio chamado R.txt
 
 pause
