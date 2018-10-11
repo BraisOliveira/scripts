ed438ea9103d6e961ba77de10366c48a5c1d0bb0
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2018-10-11 09:57:58 +0200

    Update tracert.bat

--- tracert.bat
+++ tracert.bat
@@ -22,9 +22,9 @@ tracert www.wikipedia.org >> %USERPROFILE%\Desktop\LOG_R.txt
 echo **** 10.0.129.249 **** >> %USERPROFILE%\Desktop\LOG_R.txt
 echo 1
 
-date /t >> %USERPROFILE%\Desktop\R.txt
-time /t  >> %USERPROFILE%\Desktop\R.txt
-echo  >> %USERPROFILE%\Desktop\R.txt
+date /t >> %USERPROFILE%\Desktop\LOG_R.txt
+time /t  >> %USERPROFILE%\Desktop\LOG_R.txt
+echo  >> %USERPROFILE%\Desktop\LOG_R.txt
 
 echo Proceso rematado, pode ver o resultado no log que se creou no seu escritorio chamado R.txt
 
