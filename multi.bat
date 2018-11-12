6ded76d6ea561449f890e101e32a5761630a361b
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2018-11-12 13:58:18 +0100

    Add files via upload

---
+++ multi.bat
@@ -0,0 +1,14 @@
+@echo off
+rem script para la creacion rapida de usuarios genericos
+rem BRAIS OLIVEIRA
+
+set /p nombre="Nombre de los usuarios: "
+set /p desde="Numero inicial: "
+set /p hasta="Ultimo usuario: "
+
+for /l %%v in (%desde%,1,%hasta%) do (net user %nombre%%%v %nombre%%%v /add )
+
+::wmic useraccount where name="%nombre%%%v" list /format passwordexpires=false
+
+net user 
+pause
