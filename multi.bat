72374402ff4033df93eb546552f47f710ee4034f
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2018-11-13 20:30:40 +0100

    Update multi.bat

--- multi.bat
+++ multi.bat
@@ -1,4 +1,5 @@
 @echo off
+rem ::list /format
 rem script para la creacion rapida de usuarios genericos
 rem BRAIS OLIVEIRA
 
@@ -6,9 +7,8 @@ set /p nombre="Nombre de los usuarios: "
 set /p desde="Numero inicial: "
 set /p hasta="Ultimo usuario: "
 
-for /l %%v in (%desde%,1,%hasta%) do (net user %nombre%%%v %nombre%%%v /add )
-
-::wmic useraccount where name="%nombre%%%v" list /format passwordexpires=false
+for /l %%v in (%desde%,1,%hasta%) do (net user %nombre%%%v %nombre%%%v /add
+wmic useraccount where name="%nombre%%%v" set passwordexpires=false)
 
 net user 
 pause
