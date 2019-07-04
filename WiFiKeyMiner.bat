eef6c44e73d65f3f95775a6393e01e1ce2318412
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2019-07-04 14:37:46 +0200

    Create WiFiKeyMiner.bat

---
+++ WiFiKeyMiner.bat
@@ -0,0 +1,37 @@
+@echo off
+echo -----------------------------------------------------------------------------
+echo Utilidad para la obtencion sencilla de claves de red  inalambricas en Windows
+echo Brais Oliveira Sanjurjo 												
+echo braisoliveira@protonmail.com
+echo -----------------------------------------------------------------------------
+
+rem net start Wlansvc
+
+title WiFiKeyMiner09
+color 70
+
+:inicio
+  set wifi=
+  set sc=
+  
+  net wlan show profile
+  
+echo Escoja una de las redes listadas para extraer la contraseña:
+ set /p wifi=
+echo Se creara un archivo de texto plano en su escritorio claves.txt
+ 
+cd %userprofile%\Desktop
+netsh wlan showprofile name=%wifi% key=clear | find "Contenido de la clave" >> claves.txt
+type claves.txt
+ 
+echo Pulsa C para Continuar o cualquier otra tecla para salir: 
+ set /p sc= 
+ 
+if %sc%==c goto inicio
+if %sc%==C (
+  goto inicio
+	) else (
+    	echo Hasta la proxima    
+			)   
+pause
+ 
