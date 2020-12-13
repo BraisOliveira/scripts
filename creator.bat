7826b85cb75d383004729102b7159ead7174a183
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2020-12-13 13:20:17 +0100

    Delete creator.bat

--- creator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem script para crear directorios 
-
-set /p directorio="---- ESCRIBE EL NOMBRE DEL DIRECTORIO QUE SE CREA ---->"
- 
-if exist %directorio% ( echo YA EXISTE %directorio% ) else ( mkdir %directorio% echo CREADO dir %directorio% )
-
-echo ===========================================
-pause
