c89f4b152eb48eaccb691170c2facbf12006d524
Brais Oliveira <43096370+BraisOliveira@users.noreply.github.com>
2021-11-16 23:45:00 +0100

    Update _activar_usuario_local_administrador.bat

--- _activar_usuario_local_administrador.bat
+++ _activar_usuario_local_administrador.bat
@@ -1,6 +1,6 @@
 @echo "Activando usuario local Administrador"
 @echo off
-net user Administrador Sgadmin123 /ACTIVE:YES
+net user Administrador Pass1234 /ACTIVE:YES
 wmic useraccount where (Name='Administrador'and LocalAccount= 'True') set PasswordExpires=false
  
 pause
